6accf44d5fb55d9e869009262b14618849213bc1
XoDeR <xoder.vs@gmail.com>
2025-09-24 17:18:30 +0200

    NextIntl 4.3.9 basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { prismadb } from '@/lib/prisma';
+//import { useTranslations } from 'next-intl'; // not async
+import { getTranslations } from 'next-intl/server';
 
 export default async function Home() {
   const users = await prismadb.user.findMany();
+  //const t = useTranslations('HomePage'); // not async
+  const t = await getTranslations('HomePage');
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
+        {t('title')}
       </h1>
       <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
         {users.map((user) => (
